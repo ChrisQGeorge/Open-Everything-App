10f005f7f80793de21608b45f83829b2b7e00d05
ChrisQGeorge <chrisqgeorge@gmail.com>
2023-11-29 13:26:04 -0500

    Added check to see if token exists to avoid api calls. Also, delete token on 401

--- app/components/checkAuth.tsx
+++ app/components/checkAuth.tsx
@@ -2,30 +2,34 @@ import Cookie from 'js-cookie';
 
 const checkUserAuthentication = async () => {
     const token = Cookie.get('token');
+    if(token){
+        return fetch('/api/users/me/', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(res => {
+            const status = res.status;
+            return res.json().then(data => ({
+                status: status,
+                data: data
+            }))
+        })
+        .then(result => {
+            if (result.status && result.data.username) {
+                return {"auth":true, "status":result.status, "message":"OK"}; // Authenticated
+            }
+            Cookie.remove('token');
+            return {"auth":false, "status":result.status," message":"redirect to setup"};
+        })
+        .catch(error => {
+            var errorMessage = 'Error fetching data:'+ error
 
-    return fetch('/api/users/me/', {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    })
-    .then(res => {
-        const status = res.status;
-        return res.json().then(data => ({
-            status: status,
-            data: data
-        }))
-    })
-    .then(result => {
-        if (result.status && result.data.username) {
-            return {"auth":true, "status":result.status, "message":"OK"}; // Authenticated
-        }
-        return {"auth":false, "status":result.status," message":"redirect to setup"};
-    })
-    .catch(error => {
-        var errorMessage = 'Error fetching data:'+ error
-
-        return {"auth":false, "status":500, "message":errorMessage}
-    });
-};
+            return {"auth":false, "status":500, "message":errorMessage}
+        });
+    }else{
+        return {"auth":false, "status":401, "message":"No Token"}
+    }
+}
 
 export default checkUserAuthentication;
