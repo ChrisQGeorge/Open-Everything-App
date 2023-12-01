20aab66afd719ea9dd9bd7353efc5598677a3f85
ChrisQGeorge <chrisqgeorge@gmail.com>
2023-12-01 16:31:49 -0500

    Added test dashboard components

---
+++ app/app/app/nav/pageMenu.js
@@ -0,0 +1,19 @@
+"use client"
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import React from 'react';
+
+
+export default function PageMenu() {
+    
+
+
+    return (
+        <div className="h-fit w-full rounded-xl bg-gray-200 bg-opacity-80 m-4" >
+
+            <p>Test Pane2</p>
+
+        </div>
+    )
+
+}
