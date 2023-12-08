54da6aeaa7a071b0ae5631915b06b4fa57bdfbb5
ChrisQGeorge <chrisqgeorge@gmail.com>
2023-12-08 16:29:40 -0500

    Dashboard changes

--- app/app/app/dashboard/dashboard.tsx
+++ app/app/app/dashboard/dashboard.tsx
@@ -112,25 +112,29 @@ const loadAttributes = async () => {
   if(render){
     return (
       <div className="text-black items-center justify-center min-h-screen bg-gradient-to-tl from-pink-300 via-purple-300 to-indigo-400 h-screen flex items-center justify-center">
-        <div className="rounded-xl w-11/12 h-5/6 bg-gray-200 bg-opacity-50 p-7 inline-block">
-          <div className="w-1/3 h-full m-0 float-left">
-            <div className="h-1/3 w-full m-0">
-              <CatNav />
-            </div>
-            <div className="h-1/3 w-full m-0">
-              <div className="h-fit w-full rounded-xl bg-gray-200 bg-opacity-80 m-4" >
+        <div className="rounded-xl w-11/12 h-5/6 bg-gray-200 bg-opacity-50 p-7">
+          <div className="p-0 inline-block h-full w-full overflow-visible">
+            <div className="w-1/3 h-full mr-4 p-0 float-left box-border">
+              <div className="h-1/3 w-full mb-4 box-border">
+                <CatNav />
+              </div>
+              <div className="h-1/3 w-full box-border">
+                <div className="h-full w-full rounded-xl bg-gray-200 m-0 bg-opacity-80" >
 
-                <p>Test Pane2</p>
+                  <p>Test Pane2</p>
 
+                </div>
               </div>
             </div>
-          </div>
-          <div className="w-2/3 h-full m-0 float-left">
-            <button 
-                className="px-6 py-2 mt-4 text-black bg-white rounded-lg hover:bg-blue-900 w-auto"
-                type="submit"
-                onClick={logout}
-            >Logout</button>
+            <div className="w-2/3 h-full m-0 float-left">
+              <div className="h-full w-full rounded-xl bg-gray-200 bg-opacity-80 box-border" >
+                <button 
+                    className="px-6 py-2 mt-4 text-black bg-white rounded-lg hover:bg-blue-900 w-auto"
+                    type="submit"
+                    onClick={logout}
+                >Logout</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -147,7 +151,7 @@ const loadAttributes = async () => {
 
 function CatNav(){
   return (
-      <div className="h-fit w-full rounded-xl bg-gray-200 bg-opacity-80 m-4" >
+      <div className="h-full w-full rounded-xl bg-gray-200 bg-opacity-80 m-0" >
 
           <p>Test Pane</p>
 
