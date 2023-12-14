e8629f229fce9b1ed6a89f4c4d90bb9d45e4bc1a
ChrisQGeorge <chrisqgeorge@gmail.com>
2023-12-14 13:57:49 -0500

    Changed next.js rewrites for when running npm run dev vs npm run

--- app/next.config.js
+++ app/next.config.js
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = () => {
     const rewrites = () => {
-        return [
-        {
-            source: "/api/:path*",
-            destination: "http://api:5000/:path*",
-        },
-        ];
+        if (process.env.NODE_ENV === "development") {
+            return [
+                {
+                    source: "/api/:path*",
+                    destination: "http://localhost:5000/:path*",
+                },
+                ];
+        }else{
+            return [
+                {
+                    source: "/api/:path*",
+                    destination: "http://api:5000/:path*",
+                },
+            ];
+
+        }
     };
     return {
         rewrites,
